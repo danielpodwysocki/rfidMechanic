610e58ef3041c476f871609cbbcb8da6745aed04
Daniel Podwysocki <daniel@contact24.pl>
2019-08-22 14:50:22 +0200

    22-08-2019 serial now works in a seprate thread, hooked up to the front-end

--- Main.py
+++ Main.py
@@ -3,35 +3,7 @@
 #      
 # @author: daniel
 # '''
-# from kivy.app import App
-# from kivy.uix.label import Label
-# from kivy.uix.gridlayout import GridLayout
-# from kivy.uix.button import Button
-# from kivy.uix.dropdown import DropDown
-#      
-#      
-# from Reader import Reader
-# from math import ceil
-#      
-#      
-# class GameScreen(GridLayout):
-#     arr=[]
-#     def __init__(self,players, **kwargs):
-#         super(GameScreen, self).__init__(**kwargs)
-#         self.cols=2
-#         self.dropdown = DropDown()
-#         self.dropdown.add_widget(Button(text='xd'))
-#         self.add_widget(self.dropdown)
-#         for p in range(players):
-#             self.arr.append(Label(text="player "+str(p)))
-#             self.add_widget(self.arr[p])
-#             print('lol')
-#          
-#     def test(self,index):
-#         self.arr[index].text='lol'
-#         print('xdddddddd')
-#      
-#      
+
 # class Main(App):
 #     '''
 #     This class has methods for printing the output of the Reader class 
@@ -56,7 +28,132 @@
 #          
 # t = Main()
 # t.run()
-import tkinter
+import tkinter as tk
+from math import floor
+from Reader import Reader
+import threading
+LARGE_FONT = 10
+MEDIUM_FONT = 5
+class App(tk.Tk):
+    frames={}
+    cards=[]
+    reader = Reader()
+    
+    def __init__(self,*args,**kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+        
+        container.pack(side="top", fill="both", expand = True)
+        
+        #container.grid_rowconfigure(0,weight=1) #0 - min size, weight = 1 - priority
+        #container.grid_columnconfigure(0, weight=1)
+        
+        frame = EqualDeal(container,self,2) #set start frame
+        
+        self.frames[EqualDeal] = frame # the key is the id(EqualDeal) of the class apparently (according to stack overflow). 
+        #pretty cool and i guess i'll stick with it
+        
+        
+        frame.grid(row=0,column=0, sticky="nsew") #set the grid INSIDE of the frame
+        print('lol')
+        threading.Thread(target=self.getCards).start()
+        print('xdd')
+        self.cardsUpdate()
+        self.changeFrame(EqualDeal)
+#         self.proc = Process(target=self.getCards)
+#         self.proc.start()
+    def changeFrame(self, controller):
+        frame = self.frames[controller]
+        frame.tkraise()
+    def getCards(self):
+        self.cards.append(self.reader.lastRead())
+        print(self.cards)
+        self.getCards()
+    def cardsUpdate(self):
+
+        frame = self.frames[EqualDeal]
+        frame.updateCards(self.cards)
+        print(self.cards)
+        self.after(500,self.cardsUpdate)
+
+        
+    
+        
+        
+class EqualDeal(tk.Frame):
+    '''
+    This is the frame(view) for games in which each player gets the same amount of cards
+    
+    '''
+    cardLabels=[]
+    labels=[]
+    def __init__(self, parent, controller, players):
+        self.players = players
+        tk.Frame.__init__(self, parent)
+        
+        self.spin = tk.Spinbox(master=parent, from_=2, to=15,command=self.spinUpdate)
+        self.spin.grid(pady=10,padx=100,column=2,row=0)
+        
+        self.updateLabels(players)
+        
+#         cards=[27, 1, 27]
+#         self.updateCards(cards)
+     
+    def updateCards(self,cards):
+        '''
+        function that takes an array of cards and assigns them to labels as if
+        they were dealt in the order they are in in the array 
+        '''
+        i=0
+        for l in self.cardLabels:
+            l['text']=""
+        for c in cards:
+            print(c)
+            if i==self.players:
+                i=0
+            self.cardLabels[i]['text']+=str(c)+" "
+            print(self.cardLabels[i]['text'])
+            #print(i)
+            i+=1
+            
+    def updateLabels(self,players):
+        '''
+        takes an amount of players and makes an appropriate amount of labels for them
+        '''
+        players=int(players)
+        for l in self.cardLabels:
+            l.grid_forget()
+        self.cardLabels.clear()
+        
+        for l in self.labels:
+            l.grid_forget()
+        self.labels.clear()
+        
+        for i in range(players):
+            label = tk.Label(self,text="player"+str(i)+":",font=MEDIUM_FONT)
+            cardLabel = tk.Label(self, text="cards go here ", font=MEDIUM_FONT)
+            if i == 0:
+                row = i*2
+                col = 0
+            else:
+                row = floor(i/2.0)
+                col = i % 2
+            row = i
+            label.grid(pady=10,padx=100,column=0, row=row)
+            cardLabel.grid(pady=10,padx=100, column=1, row=row)
+            self.cardLabels.append(cardLabel)
+            self.labels.append(label)
+         
+    def spinUpdate(self):
+        self.players=int(self.spin.get())
+        self.updateLabels(self.spin.get())
+        
+        
+
+
+
+myApp = App()
+myApp.mainloop()
 
 
 
