568c29587400c492667e6fe2a3e5d8bfe0d3808d
Daniel Podwysocki <daniel@contact24.pl>
2019-08-26 02:42:59 +0200

    26-08-2019

--- Main.py
+++ Main.py
@@ -29,7 +29,7 @@
 # t = Main()
 # t.run()
 import tkinter as tk
-from math import floor
+from views.EqualDeal import EqualDeal
 from Reader import Reader
 import threading
 LARGE_FONT = 10
@@ -75,83 +75,11 @@ class App(tk.Tk):
         frame.updateCards(self.cards)
         print(self.cards)
         self.after(500,self.cardsUpdate)
-
-        
     
-        
-        
-class EqualDeal(tk.Frame):
-    '''
-    This is the frame(view) for games in which each player gets the same amount of cards
-    
-    '''
-    cardLabels=[]
-    labels=[]
-    def __init__(self, parent, controller, players):
-        self.players = players
-        tk.Frame.__init__(self, parent)
-        
-        self.spin = tk.Spinbox(master=parent, from_=2, to=15,command=self.spinUpdate)
-        self.spin.grid(pady=10,padx=100,column=2,row=0)
-        
-        self.updateLabels(players)
-        
-#         cards=[27, 1, 27]
-#         self.updateCards(cards)
-     
-    def updateCards(self,cards):
-        '''
-        function that takes an array of cards and assigns them to labels as if
-        they were dealt in the order they are in in the array 
-        '''
-        i=0
-        for l in self.cardLabels:
-            l['text']=""
-        for c in cards:
-            print(c)
-            if i==self.players:
-                i=0
-            self.cardLabels[i]['text']+=str(c)+" "
-            print(self.cardLabels[i]['text'])
-            #print(i)
-            i+=1
-            
-    def updateLabels(self,players):
-        '''
-        takes an amount of players and makes an appropriate amount of labels for them
-        '''
-        players=int(players)
-        for l in self.cardLabels:
-            l.grid_forget()
-        self.cardLabels.clear()
-        
-        for l in self.labels:
-            l.grid_forget()
-        self.labels.clear()
-        
-        for i in range(players):
-            label = tk.Label(self,text="player"+str(i)+":",font=MEDIUM_FONT)
-            cardLabel = tk.Label(self, text="cards go here ", font=MEDIUM_FONT)
-            if i == 0:
-                row = i*2
-                col = 0
-            else:
-                row = floor(i/2.0)
-                col = i % 2
-            row = i
-            label.grid(pady=10,padx=100,column=0, row=row)
-            cardLabel.grid(pady=10,padx=100, column=1, row=row)
-            self.cardLabels.append(cardLabel)
-            self.labels.append(label)
-         
-    def spinUpdate(self):
-        self.players=int(self.spin.get())
-        self.updateLabels(self.spin.get())
-        
-        
+    def clearCards(self):
+        self.cards.clear()
 
-
-
+        
 myApp = App()
 myApp.mainloop()
 
