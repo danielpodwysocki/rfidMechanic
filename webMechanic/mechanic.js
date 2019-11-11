39a58619141310fa1a25dadee778051bf7a5e6e9
Daniel Podwysocki <daniel@contact24.pl>
2019-11-11 20:28:59 +0100

    Added a slider that modifies the number of players

--- webMechanic/mechanic.js
+++ webMechanic/mechanic.js
@@ -1,15 +1,32 @@
 //A script that pulls a json file and then adds divs containg information about players' hands
 
-cards=[]
+cards=[];
+playerSlider = document.getElementById("playerSlider");
+var players=4;
+function updateCards(cards){
+	cont = document.getElementById("equalDealGrid");
+	cont.innerHTML=""
+	for(i=0;i<players;i++){
+		cont.innerHTML+="<div class='player'> xd </div>"
+	}
+	
+}
 
-function updateCards(){
+function getCards(){
 	//the ip can be hardcoded, because the app is hosted on the rapsberry pi access point
 	//right now it's localhost for testing purposes
 	fetch("http://127.0.0.1:8080/getcards")
 		.then(resp=>resp.json())
 		.then(resp => {
-			console.log(resp);
+			cards=resp
+			updateCards(cards);
 		});
+}
+
+
+playerSlider.oninput = function() {
+	players = this.value;
+	updateCards(cards);
 }
 
 updateCards()
