35a4913e94ef476e28ea8a94a86e1b742ad8563a
Daniel Podwysocki <daniel@contact24.pl>
2019-11-13 20:00:44 +0100

    basic web interface works

--- webMechanic/mechanic.js
+++ webMechanic/mechanic.js
@@ -1,16 +1,23 @@
 //A script that pulls a json file and then adds divs containg information about players' hands
-
-cards=[];
+var cards=[];
 playerSlider = document.getElementById("playerSlider");
-var players=4;
-function updateCards(cards){
+var playerCount=4;
+function updateCards(playerCount){
 	cont = document.getElementById("equalDealGrid");
-	cont.innerHTML=""
-	for(i=0;i<players;i++){
-		cont.innerHTML+="<div class='player'> xd </div>"
+	cont.innerHTML="";
+	for(i=0;i<playerCount;i++){
+		cont.innerHTML+="<div class='player'> </div>";
 	}
-	
+	players = document.getElementsByClassName("player");
+	p = 0;
+	for(c=0;c<cards.length;c++){
+		if(p>=playerCount) p=0;
+		players[p].innerHTML+=cards[c]+" ";
+		p++;
+	}
 }
+
+updateCards(playerCount);
 
 function getCards(){
 	//the ip can be hardcoded, because the app is hosted on the rapsberry pi access point
@@ -18,16 +25,17 @@ function getCards(){
 	fetch("http://127.0.0.1:8080/getcards")
 		.then(resp=>resp.json())
 		.then(resp => {
-			cards=resp
-			updateCards(cards);
+			cards=resp;
+			updateCards(playerCount);
 		});
 }
 
 
 playerSlider.oninput = function() {
-	players = this.value;
-	updateCards(cards);
+	playerCount = this.value;
+	updateCards(playerCount);
 }
-
-updateCards()
+getCards();
 console.log(cards)
+
+setInterval(getCards,1000)
