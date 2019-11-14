35a9fab4c552046230098af1e52a115ae985b8fe
Daniel Podwysocki <daniel@contact24.pl>
2019-11-14 21:51:00 +0100

    added a function for converting the card number to a human-friendly name

--- webMechanic/mechanic.js
+++ webMechanic/mechanic.js
@@ -2,7 +2,21 @@
 var cards=[];
 playerSlider = document.getElementById("playerSlider");
 var playerCount=4;
+
+function toNames(card){
+	//returns a string with a human-friendly represantation of the card
+	suits = ['C','H','S','D'];
+	values = ['1','2','3','4','5','6','7','8','9','10','J','Q','K'];
+	
+	//card numbers go in CHSD order, so 1 to 13 are ace to king of clubs, 14 is the ace of hearts and so on 
+	suit = Math.ceil(card/13)-1; //-1 for the arr indexes and for the suit*13 to work
+	value = card - suit*13 -1;
+	
+	return suits[suit]+values[value];
+}
+
 function updateCards(playerCount){
+	//updates the amount of player divs and puts the translated card names inside of them
 	cont = document.getElementById("equalDealGrid");
 	cont.innerHTML="";
 	for(i=0;i<playerCount;i++){
@@ -12,7 +26,7 @@ function updateCards(playerCount){
 	p = 0;
 	for(c=0;c<cards.length;c++){
 		if(p>=playerCount) p=0;
-		players[p].innerHTML+=cards[c]+" ";
+		players[p].innerHTML+=toNames(cards[c])+" ";
 		p++;
 	}
 }
