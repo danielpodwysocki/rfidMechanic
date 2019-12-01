b80121db7d5c4efca6280794e1989ca569210b80
Daniel Podwysocki <daniel@contact24.pl>
2019-12-01 17:43:39 +0100

    added a rateHand func. Returns int, the higher it is, the better the hand

--- webMechanic/probability.js
+++ webMechanic/probability.js
@@ -1,11 +1,11 @@
 class Card{
 	constructor(card){
 		//constructor turns 'raw' card value into value (ace, 2,3...) and suit
-		//card numbers go in CHSD order, so 1 to 13 are ace to king of clubs, 14 is the ace of hearts and so on 
+		//card numbers go in CHSD order, so 1 to 13 are ace to king of clubs, 14 is the ace of hearts and so on
 
 		this.suit = Math.ceil(card/13)-1; //-1 for the arr indexes and for the suit*13 to work
 		this.value = card - this.suit*13;
-        
+
     }
 };
 
@@ -14,20 +14,20 @@ function count(hand,value){
 	//returns the amount of occurences of value in a hand
 	let count = 0;
 	for (i=0;i<hand.length;i++){
-		
+
 		if(hand[i].value==value)count++;
 	}
-	
+
 	  return count;
 }
 
 /*
- * 
+ *
  * Functions like isPair check what kind of hand you passed them
  * They return false if it's not the right type of a hand
- * If the hand matches it returns a value that helps to determine how good it is 
+ * If the hand matches it returns a value that helps to determine how good it is
  * (for pairs it will return the value of the cards that make up a pair)
- * 
+ *
  */
 
 function isFlush(hand){
@@ -55,7 +55,7 @@ function isPair(hand){
 
 function isTrip(hand){
 	//checks if a hand has a three of a kind (it can be a full house too)
-	
+
 	for(let i=0;i<hand.length;i++){
 		if(count(hand,hand[i].value)==3)
 			return hand[i].value;
@@ -64,13 +64,13 @@ function isTrip(hand){
 }
 
 function isQuad(hand){
-	//checks if a hand has a four of a kind 
+	//checks if a hand has a four of a kind
 	for(let i=0;i<hand.length;i++){
 		if(count(hand,hand[i].value)==4)
 			return hand[i].value;
 	}
 	return false;
-	
+
 }
 
 function isHouse(hand){
@@ -80,7 +80,7 @@ function isHouse(hand){
 	if(a && b){
 		if(a>b) return a;
 		else return b;
-		
+
 	}
 	else return false;
 }
@@ -89,14 +89,14 @@ function isStraight(hand){
 	hand.sort(function(a,b){
 		return a.value-b.value;
 	});
-	let i=0; //by default we want to check all cards' values, unless there's a king and an ace 
+	let i=0; //by default we want to check all cards' values, unless there's a king and an ace
 	if(hand[0].value==1 && hand[hand.length-1].value==13) i=1; //check if there's a possibility for a straight with an ace and a king
 	for(let i;i<hand.length-1;i++){ //checking if values are increasing by 1 each card, otherwise it's not a straight
 		if(hand[i].value+1 != hand[i+1].value) return false;
 	}
 	return hand[hand.length-1];
-	
-	
+
+
 }
 
 function isStraightFlush(hand){
@@ -125,11 +125,22 @@ function isRoyalFlush(hand){
 	});
 	//if the hand is a straight flush that has both an ace and a king it's got to be a royal flush
 	if(isStraightFlush(hand) && hand[0].value==1 && hand[hand.length-1].value==13) return true;
-	else return false; 
+	else return false;
 }
 
+
 function rateHand(hand){
-	handValues=[];
-	for(let i=0;i<5;i++) handValues.push(toValues(hand[i]));
-	
+	//returns an integer. the higher the integer the better the hand
+	//still need to tweak it to incorporate kicker values in the evaluation of a hand
+
+	//going from high to low to avoid giving too low a rating (a straight flush is also a straight, but it's better)
+	rateFuncs = [isRoyalFlush, isStraightFlush, isQuad, isHouse, isFlush, isStraight, isTrip, isTwoPair, isPair];
+	for(let i=0;i<rateFuncs.length){
+		if(let r = rateFuncs[i](hand)){
+			//length -i *14, so a high card value (up to 13 for the king) doesn't override a hand being better (so that a pair of kings isn't better than trip twos
+			//then we add the value returned by function from rateFuncs arr (indicates how good the hand is, so a pair of 5s returns 5 and a pair of twos returns 2)
+			return (rateFuncs.length-i)*14+r;
+		}
+	}
+
 }
